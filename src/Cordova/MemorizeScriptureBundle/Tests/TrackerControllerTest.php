74ab061646d2995366717ee8ad7446f476e7f894
cordoval <cordoval@cordoval-HP-Pavilion-dv2000-RM421LA-ABM.(none)>
2011-04-16 11:09:38 -0500

    working on testing Tracker Controller

--- src/Cordova/MemorizeScriptureBundle/Tests/TrackerControllerTest.php
+++ src/Cordova/MemorizeScriptureBundle/Tests/TrackerControllerTest.php
@@ -11,10 +11,25 @@ class TrackerControllerTest extends WebTestCase
         $client = $this->createClient();
  
         $crawler = $client->request('GET', '/tracker');
+
+        $crawler = $client->followRedirect();
  	
         $this->assertTrue($client->getResponse()->getStatusCode() == '200' );
- 
-        $this->assertTrue($crawler->filter('title:contains("Home")')->count() > 0);
+
+        $form = $crawler->selectButton('login')->form();
+        
+        $crawler = $client->submit($form, array(
+            '_username'         => 'cordoval',
+            '_password'      => 'password',
+        ));
+
+        $crawler = $client->followRedirect();
+
+        $this->assertTrue($crawler->filter('title:contains("Memorize Scripture | Home")')->count() > 0);
+
+        $this->assertRegExp('/Dashboard/', $client->getResponse()->getContent());
+        //$this->assertTrue($crawler->filter('h2')->count() > 0);
+        //$client->getResponse()->getContent()
  
         //$this->assertTrue($crawler->filter('h2:contains("Welcome! Ready to memorize Scripture? Let\'s go!")')->count() > 0);
 	//$this->assertTrue($crawler->filter('h2.post_title')->count() > 0);
