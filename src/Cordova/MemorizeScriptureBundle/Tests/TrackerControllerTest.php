c3d9f8f1e8db96da3115dca8ee454ee8a6de163d
cordoval <cordoval@cordoval-HP-Pavilion-dv2000-RM421LA-ABM.(none)>
2011-04-16 11:53:19 -0500

    submitted bug

--- src/Cordova/MemorizeScriptureBundle/Tests/TrackerControllerTest.php
+++ src/Cordova/MemorizeScriptureBundle/Tests/TrackerControllerTest.php
@@ -9,7 +9,7 @@ class TrackerControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = $this->createClient();
- 
+
         $crawler = $client->request('GET', '/tracker');
 
         $crawler = $client->followRedirect();
@@ -19,11 +19,19 @@ class TrackerControllerTest extends WebTestCase
         $form = $crawler->selectButton('login')->form();
         
         $crawler = $client->submit($form, array(
-            '_username'         => 'cordoval',
+            '_username'      => 'cordoval',
             '_password'      => 'password',
         ));
 
         $crawler = $client->followRedirect();
+        
+        $crawler = $client->request('GET', '/tracker');
+
+        //$crawler = $client->followRedirect();
+        
+        var_dump($client->getResponse()->getContent());
+        
+        //$this->assertTrue($client->getResponse()->getStatusCode() == '200' );
 
         $this->assertTrue($crawler->filter('title:contains("Memorize Scripture | Home")')->count() > 0);
 
@@ -33,6 +41,6 @@ class TrackerControllerTest extends WebTestCase
         //$client->getResponse()->getContent()
  
         //$this->assertTrue($crawler->filter('h2:contains("Welcome! Ready to memorize Scripture? Let\'s go!")')->count() > 0);
-	//$this->assertTrue($crawler->filter('h2.post_title')->count() > 0);
+    	//$this->assertTrue($crawler->filter('h2.post_title')->count() > 0);
     }
 }
