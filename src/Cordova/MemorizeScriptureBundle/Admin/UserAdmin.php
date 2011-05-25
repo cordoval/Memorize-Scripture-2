02d5c4a62b07a925d461d73effe1bfa327b3f178
Luis cordova <cordoval@gmail.com>
2011-05-25 02:17:23 -0500

    I am creating now the UserAdmin class that will be the connection with AdminBundle to my project

--- src/Cordova/MemorizeScriptureBundle/Admin/UserAdmin.php
+++ src/Cordova/MemorizeScriptureBundle/Admin/UserAdmin.php
@@ -11,7 +11,7 @@ use Cordova\MemorizeScriptureBundle\Entity\Session;
 class UserAdmin extends Admin
 {
     protected $list = array(
-        //this listing has to change ...
+        //this listing has to change
         'title' => array('identifier' => true),
         'author',
         'enabled',
